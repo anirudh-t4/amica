79018f91a1dd4a2b60d135a2068d8289092b8d26
anirudh-t4 <55625660+anirudh-t4@users.noreply.github.com>
2020-05-08 11:22:55 +0530

    Add files via upload

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/Oshi/Downloads/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/Oshi/Downloads/abhi2"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/Oshi/Downloads/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
